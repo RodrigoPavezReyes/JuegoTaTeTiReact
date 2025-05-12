345dbdbb473bf2fdacf1dff68ac830bf08ff2c71
Rodrigo Pavez <rodrigopavez.developer@gmail.com>
2025-05-12 20:40:48 -0300

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,18 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   )
 }
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+
 
 function App() {
   
@@ -30,19 +42,50 @@ const [turn, setTurn] = useState(TURNS.X)
 
 const [winner, setWinner] = useState(null) //null significa que no hay ganador, false significa que hay empate
 
+const checkWinner = (boardToCheck) => {
+  //revisamos todas las combinaciones ganadoras para ver si x o o ha ganado
+  for (const combo of WINNER_COMBOS) {
+    const [a, b, c] = combo
+    if (
+      boardToCheck[a] && 
+      boardToCheck[a] === boardToCheck[b] && 
+      boardToCheck[a] === boardToCheck[c]
+    ) {
+      return boardToCheck[a]
+    }
+  }
+  //si no hay ganador, se devuelve null
+  return null
+}
+
+const resetGame = () => {
+  setBoard(Array(9).fill(null))
+  setTurn(TURNS.X)
+  setWinner(null)
+}
+
+
 const updateBoard =(index)=>{
+  //no actualizar el tablero si ya tiene algo
+  if(board[index] || winner) return
 
-//actualizar el tablero
-  if(board[index]) return
-
-  //cambiar el turno
-
+  //actualizar el tablero
   const newBoard = [...board]
   newBoard[index] = turn
   setBoard(newBoard)
 
+    //cambiar el turno
   const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
   setTurn(newTurn)
+
+  //revisar si ya hay un ganador
+  const newWinner = checkWinner(newBoard)
+  if(newWinner) {
+    setWinner(newWinner)
+  } else if (newBoard.every((square) => square !== null)) {
+    setWinner(false) //empate
+  }
+    
 }
 
   return (
@@ -50,6 +93,7 @@ const updateBoard =(index)=>{
     <main className="board">
       <h1>TA TE TI</h1>
       <br />
+      <button onClick={resetGame}>Reset Game</button>
       <section className="game">
         {
           board.map((_, index) => {
@@ -76,8 +120,33 @@ const updateBoard =(index)=>{
           </Square>
 
       </section>
-    </main>
+
       
+        {winner !== null && (
+          <section className='winner'>
+            <div className='text'>
+              <h2>
+                {
+                winner === false 
+                ? 'Empate' 
+                : 'Ganó'
+                }
+                </h2>
+
+              <header className='win'>
+                {winner && <Square>{winner}</Square>}
+              </header>
+
+              <footer>
+                  <button onClick={resetGame}>Nueva Partida</button>
+              </footer>
+            </div>
+            
+          </section>  
+        )
+        
+      }
+    </main>      
     
   )
 }
